0e099ccdf2d6f54779e9fb4c5f6e097828ea1685
NyZaireyus Harrison <53630482+xXKamiSureyiaXx@users.noreply.github.com>
2025-09-09 23:23:11 -0700

    get chapters is a function

--- Otanet/libs/mangadex_helper.py
+++ Otanet/libs/mangadex_helper.py
@@ -73,7 +73,7 @@ class MangaDexHelper:
             chapter_path = f"{manga.get_id()}/chapter_{chapter_num}"
             base_key = f"{title}/chapter_{chapter_num}"
 
-            if chapter != manga.get_chapters[-1]:
+            if chapter != manga.get_chapters()[-1]:
                 s3_dir = self.s3_client.list_objects_v2(Bucket=self.bucket_name, Prefix=f"{base_key}/", MaxKeys=1)
                 if s3_dir['KeyCount'] > 0:       
                     print(f"Chapter Exists...Skipping: {base_key}")
